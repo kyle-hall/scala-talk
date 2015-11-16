d68da6f688bfd35c99f9591fee21906ba183be24
Kyle Hall <hallkd47@gmail.com>
2015-11-15 22:50:06 -0600

    adds different akka example

---
+++ akka-ping-pong/build.sbt
@@ -0,0 +1,8 @@
+
+name := """Akka Ping Pong"""
+
+version := "0.1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDepdencies += "com.typesafe.akka" %% "akka-actor" % "2.4.0"
---
+++ akka-ping-pong/src/main/scala/PingPong.scala
@@ -0,0 +1,49 @@
+import akka.actor._
+
+case object PingMessage
+case object PongMessage
+case object StartMessage
+case object StopMessage
+
+class Ping(pong: ActorRef) extends Actor {
+  var count = 0
+
+  def incrementAndPrint {
+    count += 1
+    println("ping")
+  }
+
+  def receive = {
+    case StartMessage =>
+      incrementAndPrint
+      pong ! PingMessage
+    case PongMessage =>
+      incrementAndPrint
+      if (count > 99) {
+        sender ! StopMessage
+        println("Ping stopped")
+        context.stop(self)
+      } else {
+        sender ! PingMessage
+      }
+  }
+}
+
+class Pong extends Actor {
+  def receive = {
+    case PingMessage =>
+      println("pong")
+      sender ! PongMessage
+    case StopMessage =>
+      println("Pong stopped")
+      context.stop(self)
+  }
+}
+
+object PingPongTest extends App {
+  val system = ActorSystem("PingPongSystem")
+  val pong = system.actorOf(Props[Pong], name = "pong")
+  val ping = sytem.actorOf(Props(new Ping(pong)), name = "ping")
+
+  ping ! StartMessage
+}
